90828cf44b7ba1f497c3753aafd3e5648202811c
Patrick <hobart.young@gmail.com>
2018-10-30 14:33:34 -0400

    upgrade spark and zeppelin

---
+++ hadoop/load_fakeCDM.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## We grab the fake CDM v3 data available from these folks:
+## https://github.com/LHSNet/PCORNet-CDM/tree/master/PCORNet-CDM-v3/fake-data
+mkdir CDM;cd CDM
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/diagnosis.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/enrollment.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/lab_result_cm.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/procedures.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/demographic.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/encounter.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/harvest_c12umi.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/prescribing.csv
+wget https://github.com/LHSNet/PCORNet-CDM/raw/master/PCORNet-CDM-v3/fake-data/vital.csv
+
+# change ownership to user "hadoop" and move to /home/hadoop
+cd ..;chown hadoop.hadoop CDM
+mv CDM /home/hadoop
+
+# user "hadoop" copy to hdfs /data
+sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /home/hadoop/CDM /data"
+
+# Clean up
+sudo -u hadoop -H sh -c "rm -fr /home/hadoop/CDM"
--- hadoop/startup.sh
+++ hadoop/startup.sh
@@ -8,12 +8,7 @@ service hadoop start
 
 useradd -g hadoop storm
 useradd -g hadoop nifi
-useradd -g hadoop hive
-sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user; /usr/local/hadoop/bin/hdfs dfs -mkdir /user/nifi; /usr/local/hadoop/bin/hdfs dfs -chown nifi:hdfs /user/nifi; /usr/local/hadoop/bin/hdfs dfs -chmod -R 777 /user/nifi"
+sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user; /usr/local/hadoop/bin/hdfs dfs -mkdir /user/nifi; /usr/local/hadoop/bin/hdfs dfs -chown nifi:hadoop /user/nifi"
 sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /data; /usr/local/hadoop/bin/hdfs dfs -chown storm:hadoop /data"
-sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user/hive; /usr/local/hadoop/bin/hdfs dfs -mkdir /user/hive/warehouse; /usr/local/hadoop/bin/hdfs dfs -chown hive:hadoop /user/hive; /usr/local/hadoop/bin/hdfs dfs -chmod -R 777 /user/hive"
 
-echo "export HADOOP_HOME=${HADOOP_HOME}" > /usr/local/hive/conf/hive-env.sh
-sudo -u hadoop -H sh -c "$HIVE_HOME/bin/schematool -initSchema -dbType postgres"
-sudo -u hadoop -H sh -c cd; zeppelin-daemon.sh start
-sudo -u hadoop -H sh -c "$HIVE_HOME/bin/hive --service hiveserver2"
+sudo -u hadoop -H sh -c "cd; $ZEPPELIN_HOME/bin/zeppelin-daemon.sh start"
