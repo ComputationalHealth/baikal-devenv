b9e4c24e5a7bc5768676744d116a183f62f0df39
Patrick <hobart.young@gmail.com>
2019-02-05 16:25:18 -0500

    cd redundant in last line. removed.

--- hadoop/startup.sh
+++ hadoop/startup.sh
@@ -11,4 +11,4 @@ useradd -g hadoop nifi
 sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user; /usr/local/hadoop/bin/hdfs dfs -mkdir /user/nifi; /usr/local/hadoop/bin/hdfs dfs -chown nifi:hadoop /user/nifi"
 sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /data; /usr/local/hadoop/bin/hdfs dfs -chown storm:hadoop /data"
 
-sudo -u hadoop -H sh -c "cd; $ZEPPELIN_HOME/bin/zeppelin-daemon.sh start"
+sudo -u hadoop -H sh -c "$ZEPPELIN_HOME/bin/zeppelin-daemon.sh start"
